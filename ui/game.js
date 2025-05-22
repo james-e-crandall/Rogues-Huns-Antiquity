524452dcf70f17aa0a878549d03bfaf81daa7444
james-e-crandall <james.e.crandall@gmail.com>
2025-05-21 21:06:04 -0500

    working with issues

---
+++ ui/game.js
@@ -0,0 +1,21 @@
+window.UnitPortraitVisualRemaps = {
+    ...(window.UnitPortraitVisualRemaps || {}),
+    'UNIT_KANDAR': { //your unit type
+        unitType: 'UNIT_HORSEMAN',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    },
+    'UNIT_KANDAR_2': { //your unit type
+        unitType: 'UNIT_HORSEMAN',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    },
+    'UNIT_KANDAR_3': { //your unit type
+        unitType: 'UNIT_HORSEMAN',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    }
+}
+
+
+
+
+
+
