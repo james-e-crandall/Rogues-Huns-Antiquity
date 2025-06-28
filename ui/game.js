bcf2586988e57d3affdcbccea00721774c01c3a3
james-e-crandall <james.e.crandall@gmail.com>
2025-06-27 23:04:11 -0500

    ROG

--- ui/game.js
+++ ui/game.js
@@ -1,18 +1,18 @@
 window.UnitPortraitVisualRemaps = {
     ...(window.UnitPortraitVisualRemaps || {}),
-    'UNIT_KANDAR': { //your unit type
+    'UNIT_KANDAR_ROG': { //your unit type
         unitType: 'UNIT_HORSEMAN',
         background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
     },
-    'UNIT_KANDAR_2': { //your unit type
+    'UNIT_KANDAR_ROG_2': { //your unit type
         unitType: 'UNIT_HORSEMAN',
         background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
     },
-    'UNIT_KANDAR_3': { //your unit type
+    'UNIT_KANDAR_ROG_3': { //your unit type
         unitType: 'UNIT_HORSEMAN',
         background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
     },
-    'UNIT_TUNKUR': { //your unit type
+    'UNIT_TUNKUR_ROG': { //your unit type
         unitType: 'UNIT_BALLISTA',
         background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
     }
